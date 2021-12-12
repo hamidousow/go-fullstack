f9cbed5bbeab700e88055e5e85e01275a5878d2c
Hamidou.S <86967645+hamidousow@users.noreply.github.com>
2021-12-12 21:22:09 +0100

    fix nodemon server and install express

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
-const http = require("http"); //importer le package http de node  
+const http = require('http'); //importer le package http de node 
 
+// const server = http.createServer((req, res) => {
+//     res.end('voila la reponse du serveur');
+// }); 
 
-const server = http.createServer((req, res) => {
-    res.end('voila la reponse du serveur');
-}); 
+const app = require('./app');
+
+app.set('port', process.env.PORT || 3000 );
+const server = http.createServer(app)
 
 server.listen(process.env.PORT || 3000); // process.env.PORT > variable environnement 
