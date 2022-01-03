116beac6b45a13470bae0faa95669c228ec2c775
Hamidou.S <86967645+hamidousow@users.noreply.github.com>
2022-01-03 09:17:06 +0100

    routes and controllers

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -10,9 +10,26 @@ exports.createThing = (req, res, next) => {
     .catch(() => res.status(400).json({ error }))
 }
 
-exports.modifyThing = (req, res, next) => {
+exports.getOneThing = (req, res, next) => {
     Thing.findOne({ _id: req.params.id })
         .then(thing => res.status(200).json(thing))
         .catch(() => res.status(404).json({ error }))
 }
 
+exports.modifyThing = (req, res, next) => {
+    Thing.updateOne({ _id: req.params.id}, {...req.body, _id: req.params.id})
+    .then(() => res.status(200).json({ message: 'objet modifié !'}))
+    .catch(() => res.status(400).json({ error }))
+}
+
+exports.deleteThing = (req, res, next) => {
+    Thing.deleteOne({ _id: req.params.id})
+    .then(() => res.status(200).json({ message: 'objet supprimé !'}))
+    .catch(() => res.status(400).json({ error }))    
+}
+
+exports.getAllStuff = (req, res, next) => {
+    Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(() => res.status(400).json({ error }))
+}
--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,41 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const Thing = require('../models/thing');
+const stuffCtrl = require('../controllers/stuff');
 
-
-router.post('/', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-        ...req.body
-    });
-    thing.save() 
-    .then(()=> res.status(201).json({ message: 'Object bien enregistré' }))
-    .catch(() => res.status(400).json({ error }))
-})
-
-router.get('/:id', (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-        .then(thing => res.status(200).json(thing))
-        .catch(() => res.status(404).json({ error }))
-})
-
-router.put('/:id', (req, res, next) => {
-    Thing.updateOne({ _id: req.params.id}, {...req.body, _id: req.params.id})
-    .then(() => res.status(200).json({ message: 'objet modifié !'}))
-    .catch(() => res.status(400).json({ error }))
-});
-
-router.delete('/:id', (req, res, next) => {
-    Thing.deleteOne({ _id: req.params.id})
-    .then(() => res.status(200).json({ message: 'objet supprimé !'}))
-    .catch(() => res.status(400).json({ error }))
-    
-})
-
-router.get('/', (req, res, next) => {
-    Thing.find()
-    .then(things => res.status(200).json(things))
-    .catch(() => res.status(400).json({ error }))
-});
+router.post('/', stuffCtrl.createThing);
+router.put('/:id', stuffCtrl.modifyThing);
+router.delete('/:id', stuffCtrl.deleteThing);
+router.get('/:id', stuffCtrl.getOneThing);
+router.get('/', stuffCtrl.getAllStuff);
 
 module.exports = router;
